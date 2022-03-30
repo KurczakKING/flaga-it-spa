f6d460de5a27586e0f287af6029e413e6430d2ab
KurczakKING <monikabflaga@gmail.com>
2022-03-30 19:17:55 +0200

    Updating .hbs

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const port = 3000;
 const app = express();
 app.set("view engine", "hbs");
 app.get("/", function (request, response) {
-  response.render("index");
+  response.render("index", {
+    pageTitle: "Witaj w IT SPA!",
+  });
 });
 
 app.listen(port, (error) => {
@@ -17,5 +19,7 @@ app.listen(port, (error) => {
   console.log("Serwer działa na porcie ", port);
 });
 
-const path = require('path')
-app.use('/assets', express.static(path.join(__dirname, "./assets")));
+const path = require("path");
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+
+app.use("/src/js", express.static(path.join(__dirname, "./src/js")));
---
+++ src/js/functions.js
@@ -0,0 +1,8 @@
+alert("Hello World");
+
+/*
+const example = () => {
+  const fromInput = document.getElementsById("example").value;
+  document.getElementsByTagName("p")[0].innerText = fromInput;
+};
+document.getElementById("action").addEventListener("click", example);*/
